7a97cd6c37ae99bda8430bc575f1614d97567d39
Dima Kochetov <dima.kochetov.70@gmail.com>
2026-01-08 12:39:10 +0100

    Update request data script to request SoC

--- data_requests/request_data.py
+++ data_requests/request_data.py
@@ -34,7 +34,7 @@ def prepare_dir(dir_name):
         os.makedirs(dir_name)
 
 
-def build_curl_command(cookie, date, timestamp_ms):
+def build_curl_overview(cookie, date, timestamp_ms):
     return [
         "curl",
         f"https://uni001eu5.fusionsolar.huawei.com/rest/pvms/web/station/v3/overview/energy-balance?"
@@ -47,6 +47,23 @@ def build_curl_command(cookie, date, timestamp_ms):
         "-H", "accept: application/json, text/javascript, */*; q=0.01",
         "-H", f"cookie: {cookie}",
         "-H", "referer: https://uni001eu5.fusionsolar.huawei.com/uniportal/pvmswebsite/assets/build/cloud.html",
+        "-H", "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+        "--compressed"
+    ]
+
+
+def build_curl_soc(cookie, target_ts, curr_ts):
+    return [
+        "curl",
+        f"https://uni001eu5.fusionsolar.huawei.com/rest/pvms/web/device/v1/device-history-data?"
+        f"showDst=true&"
+        f"signalIds=30007&"
+        f"deviceDn=NE%3D168938049&"
+        f"date={target_ts}&"
+        f"_={curr_ts}",
+        "-H", "accept: application/json, text/javascript, */*; q=0.01",
+        "-H", f"cookie: {cookie}",
+        "-H", "referer: https://uni001eu5.fusionsolar.huawei.com/uniportal/pvmswebsite/assets/build/cloud.html?app-id=smartpvms&instance-id=smartpvms&zone-id=region-1-0fad1bea-d877-42b0-88c9-8b1cbe4a08a3",
         "-H", "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
         "--compressed"
     ]
@@ -67,21 +84,39 @@ def save_response(response_text, filename):
 
 def main():
     start_date = "2025-03-13"
-    end_date = "2025-12-20"
+    end_date = "2026-01-08"
     date_format = "%Y-%m-%d"
-    cookie_file = "cookie.txt"
+    cookie_overview_file = "cookie_overview.txt"
+    cookie_soc_file = "cookie_soc.txt"
     save_dir = "../data/days-range"
 
     date_list = generate_date_range(start_date, end_date, date_format)
-    timestamp_ms = int(time.time()) * 1000
-    cookie = read_cookie(cookie_file)
-
-    prepare_dir(save_dir)
+    curr_ts = int(time.time()) * 1000
+    cookie_overview = read_cookie(cookie_overview_file)
+    cookie_soc = read_cookie(cookie_soc_file)
 
     for date in date_list:
-        cmd = build_curl_command(cookie, date, timestamp_ms)
-        response = run_curl_cmd(cmd)
-        save_response(response, os.path.join(save_dir, f"{date}.json"))
+        date_save_dir = os.path.join(save_dir, date)
+        prepare_dir(date_save_dir)
+
+        # for the previous day
+        dt = datetime.strptime(date, date_format) + timedelta(days=1)
+        date_ts = int(dt.timestamp() * 1000)
+
+        cmd_overview = build_curl_overview(cookie_overview, date, curr_ts)
+        cmd_soc = build_curl_soc(cookie_soc, date_ts, curr_ts)
+
+        response_overview = run_curl_cmd(cmd_overview)
+        response_soc = run_curl_cmd(cmd_soc)
+
+        save_response(
+            response_overview,
+            os.path.join(date_save_dir, f"overview.json"),
+        )
+        save_response(
+            response_soc,
+            os.path.join(date_save_dir, f"soc.json"),
+        )
 
 
 if __name__ == '__main__':
